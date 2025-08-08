b8942d93e94d29a44bf64034092c878f20641384
Tanner Coker <tanner.coker@hotmail.com>
2025-08-07 21:58:05 -0700

    Fix routing error: ensure App.tsx (with BrowserRouter) is used by index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorker';
 
